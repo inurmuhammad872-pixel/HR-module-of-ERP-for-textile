db4a4897274f5ff8cd647fe2044494d191a33f2f
Nurmuhammad <flastdtb@yandex.ru>
2025-12-22 18:20:04 +0500

    Some changes

--- Adminapp/views.py
+++ Adminapp/views.py
@@ -76,4 +76,11 @@ class XodimlarStatisticsAPIView(APIView):
         })
     
 
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@csrf_exempt
+@api_view(['POST'])
+def login_view(request):
+    return Response({"success": True})
--- Administrator/urls.py
+++ Administrator/urls.py
@@ -29,5 +29,3 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-
